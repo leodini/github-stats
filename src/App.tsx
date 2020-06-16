d0e6bf447c1f4db619ee2d4f66b6419252642c22
leodini <leodini@hotmail.com>
2020-06-15 23:20:54 -0300

    types in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import React, { useEffect } from "react";
-// import GhPolyglot from "gh-polyglot";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 
 const GhPolyglot = require("gh-polyglot");
 
+interface githubStats {
+  label: String;
+  value: Number;
+  color: String;
+}
+
 const App: React.FC = () => {
+  const [githubUserStats, setGithubUserStats] = useState<Array<githubStats>>(
+    []
+  );
+
   useEffect(() => {
+    getUserStats();
+  }, []);
+
+  const getUserStats = async () => {
     const me = new GhPolyglot("leodini");
     console.log(me);
-    me.userStats((err: Error, stats: Object) => {
+    await me.userStats((err: Error, stats: Array<githubStats>) => {
       if (err) {
         console.error("Error:", err);
       }
       console.log(stats);
+      setGithubUserStats(stats);
     });
-    me.getAllRepos((err: Error, stats: Object) => {
+    console.log(githubUserStats);
+    await me.getAllRepos((err: Error, stats: Object) => {
       console.log(stats);
     });
-  }, []);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>github-stats</h2>
+      {githubUserStats &&
+        githubUserStats.map((stat) => (
+          <div className="">
+            <p>{stat.label}</p>
+          </div>
+        ))}
     </div>
   );
 };
